850d5e7c6afe088bb954ea4053760c1820c47a9e
Hudi_Indrawan <79536179+Kororo212@users.noreply.github.com>
2022-06-19 22:21:33 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Header from './components/Header.js';
+import Contact from './components/Contact';
+
+
 
 const App = () => {
-  // Masukkan Header dan Contact ke dalam div App
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    // Masukkan Header dan Contact ke dalam div App
+    return ( <><div className = "App" ><Header /></div>
+    <div className='container'>
+        <Contact dataKontak= {contacts[0]}/>
+        <Contact dataKontak= {contacts[1]}/>
+        <Contact dataKontak= {contacts[2]}/>
+        <Contact dataKontak={contacts[3]}/>
+        <Contact dataKontak={contacts[4]}/>
+        <Contact dataKontak= {contacts[5]}/>
+    </div></>
+    );
 }
 
 export default App;
